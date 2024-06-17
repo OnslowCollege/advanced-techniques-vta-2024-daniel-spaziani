8d3110fd3db4a3fd07a83f09d2fb6be8dd517e6e
DJS6 <99764494+DJS6@users.noreply.github.com>
2024-06-17 01:02:42 +0000

    Created Card Class
    Basic logic written for class, storage for information written, and methods will be written as soon as some more of the programme is fleshed out.

--- main.py
+++ main.py
@@ -1,13 +1,40 @@
 """
 Main.
 
-Created by: NAME
-Date: DATE
+Created by: Daniel Spaziani
+Date: 17/6/24
 """
 
-# Enter your code here
-name: str = input("Enter your name: ")
-if name:
-    print(f"Hello, {name}!")
-else:
-    print("Error getting name.")
+
+# Enter your code here.
+class Card:
+    """Template for a card, stores all required information."""
+
+    def __init__(self, number: int, colour: str, function) -> None:
+        """Saves all info to function."""
+        self.number = str(number)
+        self.colour = colour
+        self.function = function
+
+    def calculate_change(self, card_placed_on: Card) -> bool:
+        """Based on card that this one is being placed on,
+        calculates what values to change."""
+        # Starts by working out whether the card play is valid or not.
+        if (
+            self.colour != card_placed_on.colour
+            and self.number != card_placed_on.number
+        ):
+            # If neither attribute matches, then it's not valid.
+            valid = False
+        else:
+            # Else it is valid.
+            valid = True
+        if valid == True:
+            # TODO: This section.
+            # The programme now knows that the move is valid.
+            # Now works out what exact operation to make.
+            # Then updates the card on top to the new one.
+            # First removes the card from the user's hand.
+            # Then checks if it's a specific purpose card, and checks its use.
+            # If it's a colour change card, then passes that on.
+            # Else if it's a plus two or four card, then passes on that value.
