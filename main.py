2c6ec49078cc94bf6d955178fe86d87c265dcd73
DJS6 <99764494+DJS6@users.noreply.github.com>
2024-06-18 23:18:42 +0000

    Created game function
    Game function basic planning.

--- main.py
+++ main.py
@@ -5,14 +5,18 @@ Created by: Daniel Spaziani
 Date: 17/6/24
 """
 
-
 # Enter your code here.
+
+# Constant for number of cards players start with in their hands.
+NUMBER_OF_CARDS_IN_HAND = 7
+
+
 class Card:
     """Template for a card, stores all required information."""
 
     def __init__(self, number: int, colour: str, function) -> None:
         """Saves all info to function."""
-        self.number = str(number)
+        self.number = int(number)
         self.colour = colour
         self.function = function
 
@@ -29,7 +33,7 @@ class Card:
         else:
             # Else it is valid.
             valid = True
-        if valid == True:
+        if valid is True:
             # TODO: This section.
             # The programme now knows that the move is valid.
             # Now works out what exact operation to make.
@@ -38,3 +42,14 @@ class Card:
             # Then checks if it's a specific purpose card, and checks its use.
             # If it's a colour change card, then passes that on.
             # Else if it's a plus two or four card, then passes on that value.
+            pass
+
+
+# Constant for deck of cards, with card items to create deck list to pick from.
+DECK: list[Card] = [""]
+
+
+def game():
+    """Main game function."""
+    # Starts by dealing cards to both the user and the other player.
+    # Gets seven random cards from the deck, and gives them to each player.
